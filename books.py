11cea59e283b4ad01e4658f7c42897f410e7b472
akadamboy <abhijith.mampara@gmail.com>
2025-07-23 20:34:34 +0530

    first fast api call to show list of books

---
+++ books.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+
+
+@app.get("/")
+async def home():
+    return {'home page'}
+
+@app.get("/welcome")
+async def greet():
+    return {'Hi Good morning!'}
+
+@app.get("/books")
+async def all_books():
+    return BOOKS 

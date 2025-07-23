2e4557964f8bd09f7aa410a9140deb3edfcbd113
akadamboy <abhijith.mampara@gmail.com>
2025-07-23 22:21:16 +0530

    learned dynamic parameter passing through URL

--- books.py
+++ books.py
@@ -18,10 +18,23 @@ BOOKS = [
 async def home():
     return {'home page'}
 
-@app.get("/welcome")
+@app.get("/welcome") #welcome page
 async def greet():
     return {'Hi Good morning!'}
 
-@app.get("/books")
-async def all_books():
+@app.get("/books") #return all the books
+async def read_all_books():
     return BOOKS 
+
+# passing dynamic parameter through URL
+
+@app.get("/books/{book_name}")
+async def read_book_by_name(book_name):
+    book_name = book_name
+
+    #we can write the retun statement in multiple ways
+    # return ( f"book name is {book_name}" )  # using fstriing
+    # return f"book name is {book_name}"   #fstring
+    # return "book name is "+book_name
+    return {"book name" :book_name} # returns a dictionary object
+

ca4eccec57deea801301a12e30033b8d400fc2be
akadamboy <abhijith.mampara@gmail.com>
2025-07-25 01:02:35 +0530

    learned post method and body function

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
@@ -54,8 +54,29 @@ async def get_book_by_name(book_name:str): # we can give explicit type. in this 
 
 @app.get("/book/")  # nothing after /book/ means we need to pass the variable ass query parameter
 async def get_book_by_category(category: str): # query parameter variable name should match the function parameter name
-    for book in BOOKS:
+    books_list = []
+    for book in BOOKS:   
         if book.get('category').casefold() == category.casefold():
-            return book
+            books_list.append(book)
+    return books_list
 
+#----- Using QUERY PARAMETER along with PATH VARIABLE ----#
+
+@app.get("/book/{author}/")
+async def get_book_by_category_and_author(author: str, category: str):
+    for book in BOOKS:
+        if (book.get('author').casefold() == author.casefold() and  book.get('category').casefold() == category.casefold()):
+            return book
+    
+
+
+#----- Learning POST REQUEST-------#
+
+# to send data through body import body from fast api
+
+
+@app.post("/books/addBook")
+async def add_book(newBook = Body()):
+    BOOKS.append(newBook)
+    return "book added"
 

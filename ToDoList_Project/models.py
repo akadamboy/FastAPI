fb559cdbc7be14984b1c151f56042cde6329a5cd
akadamboy <abhijith.mampara@gmail.com>
2025-08-07 11:42:10 +0530

    created models.py and defined the table coloumns in Todos class

---
+++ ToDoList_Project/models.py
@@ -0,0 +1,17 @@
+#model file used to define the tables in the database.
+
+from database import Base
+from sqlalchemy import Boolean, Column, Integer, String
+
+
+class Todos(Base):  # class inherits the base we declared in the database.py file using declarative_base()
+    __tablename__ = 'todos' 
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete_status = Column(Boolean, default= False)
+
+
+

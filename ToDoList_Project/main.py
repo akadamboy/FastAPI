aa780e607db9918beac7e3f0bd90fb8e1cfa4246
akadamboy <abhijith.mampara@gmail.com>
2025-08-19 23:00:24 +0530

    created a function to open db connection and close it. Then created dependency injection using annotated. used a get function to get all data from the database

--- ToDoList_Project/main.py
+++ ToDoList_Project/main.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI
-from database import engine
+from typing import Annotated
+from fastapi import FastAPI, Depends
+from database import SessionLocal, engine
+from sqlalchemy.orm import Session
 import models
+from models import Todos
 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind= engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)] # assigning Annotated to a variable
+
+
+@app.get("/")
+# async def get_all(db: Annotated[Session, Depends(get_db)]): #we can give the Annotated type here directly or we can assign it to a variable
+async def get_all(db: db_dependency):
+    return db.query(Todos).all()
+    

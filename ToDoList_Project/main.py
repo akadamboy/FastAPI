75fd57ab389641bfb96396d85089626d594572cf
akadamboy <abhijith.mampara@gmail.com>
2025-08-20 21:53:48 +0530

    created an end point to return todo item by id added status codes and exceptions

--- ToDoList_Project/main.py
+++ ToDoList_Project/main.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path, status
 from database import SessionLocal, engine
 from sqlalchemy.orm import Session
 import models
@@ -21,8 +21,16 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)] # assigning Annotated to a variable
 
 
-@app.get("/")
+@app.get("/", status_code= status.HTTP_200_OK)
 # async def get_all(db: Annotated[Session, Depends(get_db)]): #we can give the Annotated type here directly or we can assign it to a variable
 async def get_all(db: db_dependency):
     return db.query(Todos).all()
     
+
+@app.get("/todo/{todo_id}", status_code= status.HTTP_200_OK)
+async def get_todo_by_id(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail="todo id not found")

60dcb320be2be0fcc541ea25664c1d71858014f5
akadamboy <abhijith.mampara@gmail.com>
2025-08-22 22:22:17 +0530

    moved all todo end point to seperate file and added router for both todo and auth file in main file

--- ToDoList_Project/main.py
+++ ToDoList_Project/main.py
@@ -1,92 +1,108 @@
 from typing import Annotated, Optional
 from fastapi import FastAPI, Depends, HTTPException, Path, status
 from pydantic import BaseModel, Field
+
+from routers import auth, todosmai
 from database import SessionLocal, engine
 from sqlalchemy.orm import Session
 import models
 from models import Todos
 
 
+
 app = FastAPI()
 
 models.Base.metadata.create_all(bind= engine)
 
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-db_dependency = Annotated[Session, Depends(get_db)] # assigning Annotated to a variable
-
-#creating a class to verify the data coming from post request body
-
-
-class TodoRequest(BaseModel):
-    # id : Optional[int]  no need to add id here cause sql alchemy will take care of this
-    title : str = Field(min_length=3)
-    description : str = Field(min_length=5, max_length= 100)
-    priority : int = Field(gt=0, le=5)
-    complete_status : bool = Field(default= False)
+app.include_router(auth.router)
+app.include_router(todos.router)
 
 
 
 
+# -------------------------------------------------------------#
+#all the below code is commented to implement routing and make the code clean
 
 
-@app.get("/", status_code= status.HTTP_200_OK)
-# async def get_all(db: Annotated[Session, Depends(get_db)]): #we can give the Annotated type here directly or we can assign it to a variable
-async def get_all(db: db_dependency):
-    return db.query(Todos).all()
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+# db_dependency = Annotated[Session, Depends(get_db)] # assigning Annotated to a variable
+
+# #creating a class to verify the data coming from post request body
+
+
+# class TodoRequest(BaseModel):
+#     # id : Optional[int]  no need to add id here cause sql alchemy will take care of this
+#     title : str = Field(min_length=3)
+#     description : str = Field(min_length=5, max_length= 100)
+#     priority : int = Field(gt=0, le=5)
+#     complete_status : bool = Field(default= False)
+
+
+
+
+
+
+# @app.get("/", status_code= status.HTTP_200_OK)
+# # async def get_all(db: Annotated[Session, Depends(get_db)]): #we can give the Annotated type here directly or we can assign it to a variable
+# async def get_all(db: db_dependency):
+#     return db.query(Todos).all()
     
 
-@app.get("/todo/{todo_id}", status_code= status.HTTP_200_OK)
-async def get_todo_by_id(db: db_dependency, todo_id: int = Path(gt=0)):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+# @app.get("/todo/{todo_id}", status_code= status.HTTP_200_OK)
+# async def get_todo_by_id(db: db_dependency, todo_id: int = Path(gt=0)):
+#     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
 
-    if todo_model is not None:
-        return todo_model
-    raise HTTPException(status_code=404, detail="todo id not found")
-
-
-@app.post("/todo/create", status_code = status.HTTP_201_CREATED)
-async def create_todo(todo_item: TodoRequest, db: db_dependency): #take data from request body and create database session
-
-    todo_model = Todos(**todo_item.model_dump())  #map the values to the todo class variables and create a data model to add to the database
-    db.add(todo_model) #insert the model to database
-    db.commit() #commits the insert
+#     if todo_model is not None:
+#         return todo_model
+#     raise HTTPException(status_code=404, detail="todo id not found")
 
 
-@app.put("/todo/update/{todo_id}", status_code= status.HTTP_204_NO_CONTENT)
-async def update_todo(db: db_dependency, 
-                      #todo_id: int = Path(gt=0), # Can not give path validation above the request body will give error
-                      todo_request: TodoRequest,
-                      todo_id: int = Path(gt=0)):
+# @app.post("/todo/create", status_code = status.HTTP_201_CREATED)
+# async def create_todo(todo_item: TodoRequest, db: db_dependency): #take data from request body and create database session
 
-    todo_model =  db.query(Todos).filter(Todos.id == todo_id).first()
-    if todo_model is None:
-        raise HTTPException(status_code=404, detail="item not found in db")
+#     todo_model = Todos(**todo_item.model_dump())  #map the values to the todo class variables and create a data model to add to the database
+#     db.add(todo_model) #insert the model to database
+#     db.commit() #commits the insert
+
+
+# @app.put("/todo/update/{todo_id}", status_code= status.HTTP_204_NO_CONTENT)
+# async def update_todo(db: db_dependency, 
+#                       #todo_id: int = Path(gt=0), # Can not give path validation above the request body will give error
+#                       todo_request: TodoRequest,
+#                       todo_id: int = Path(gt=0)):
+
+#     todo_model =  db.query(Todos).filter(Todos.id == todo_id).first()
+#     if todo_model is None:
+#         raise HTTPException(status_code=404, detail="item not found in db")
     
-    todo_model.title = todo_request.title
-    todo_model.description = todo_request.description
-    todo_model.priority = todo_request.priority
-    todo_model.complete_status = todo_request.complete_status
+#     todo_model.title = todo_request.title
+#     todo_model.description = todo_request.description
+#     todo_model.priority = todo_request.priority
+#     todo_model.complete_status = todo_request.complete_status
 
-    db.add(todo_model)
-    db.commit()
-    return "update success"
+#     db.add(todo_model)
+#     db.commit()
+#     return "update success"
 
 
-@app.delete("/todo/delete/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_todo(db: db_dependency, todo_id:int = Path(gt=0)):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
-    if todo_model is None:
-        raise HTTPException(status_code=404 , detail="todo with id not found in db")
-    # db.query(Todos).filter(Todos.id == todo_id).delete() 
-    db.delete(todo_model)
-    db.commit()
+# @app.delete("/todo/delete/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+# async def delete_todo(db: db_dependency, todo_id:int = Path(gt=0)):
+#     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+#     if todo_model is None:
+#         raise HTTPException(status_code=404 , detail="todo with id not found in db")
+
+# #We can use both the method to delte items from db
+# # if you want to bulk delete the first method will be faster than passing the whole model to the delete function
+
+#     # db.query(Todos).filter(Todos.id == todo_id).delete()  
+#     db.delete(todo_model)
+#     db.commit()
 
     
     
